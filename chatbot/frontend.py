79b99d73b5e888ed050ac088fefe19013991f8fe
abhiji-th <abhijithc366@gmail.com>
2026-07-23 20:21:02 +0530

    Add chatbot with UI

---
+++ chatbot/frontend.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from backend import chatbot
+from langchain.messages import HumanMessage
+
+thread_id = 1 
+CONFIG = {'configurable': {'thread_id': thread_id}}
+
+
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = []
+
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
+
+user_message = st.chat_input('Type here')
+
+if user_message:
+
+    st.session_state['message_history'].append({'role': 'user', 'content': user_message})
+    with st.chat_message('user'):
+        st.text(user_message)
+
+    response = chatbot.invoke({'messages': [HumanMessage(content=user_message)]}, config = CONFIG)
+    ai_message = response['messages'][-1].content
+
+    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})                                           
+    with st.chat_message('assistant'):
+        st.text(ai_message)
